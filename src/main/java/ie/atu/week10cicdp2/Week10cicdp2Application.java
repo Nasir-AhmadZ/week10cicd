693fbd5f692e0d022cdaa9c2c22c5be1401cca45
Nasir Ahmad <G00419630@atu.ie>
2024-11-27 15:44:22 +0000

    initial

---
+++ src/main/java/ie/atu/week10cicdp2/Week10cicdp2Application.java
@@ -0,0 +1,13 @@
+package ie.atu.week10cicdp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week10cicdp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week10cicdp2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week10cicdp2/Week10cicdp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week10cicdp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week10cicdp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
